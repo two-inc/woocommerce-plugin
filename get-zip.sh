146707eecbd1351f596c90faa14d02e7122a9da5
Harry <hnguyen@tillit.ai>
2021-06-09 17:02:02 +0700

    rename slug to tillit-payment-gateway for file names only

--- get-zip.sh
+++ get-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-zip -r woocommerce-gateway-tillit.zip 'assets' 'class' 'views' 'readme.txt' '*.php' '*.pot' '*.mo' '*.po'
+zip -r tillit-payment-gateway.zip 'assets' 'class' 'views' 'readme.txt' '*.php' '*.pot' '*.mo' '*.po'
 printf "\n\n"
 read -n 1 -s -r -p "Zipping completed. Please press any key to exit"
