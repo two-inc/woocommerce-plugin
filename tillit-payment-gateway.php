4f60261fda71d53f304a67ea17561c2d8527480b
harry <hnguyen@tillit.ai>
2021-11-26 22:32:55 +0700

    new wording flow

--- tillit-payment-gateway.php
+++ tillit-payment-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: Two. - BNPL for businesses
  * Plugin URI: https://two.inc
  * Description: Integration between WooCommerce and Two.
- * Version: 2.4.1
+ * Version: 2.4.2
  * Author: Two.
  * Author URI: https://two.inc
  * Text Domain: twoinc-payment-gateway
@@ -91,7 +91,7 @@ function wc_twoinc_add_to_gateways($gateways)
  */
 function wc_twoinc_enqueue_styles()
 {
-    wp_enqueue_style('twoinc-payment-gateway-css', WC_TWOINC_PLUGIN_URL . '/assets/css/twoinc.css', false, '1.2.0');
+    wp_enqueue_style('twoinc-payment-gateway-css', WC_TWOINC_PLUGIN_URL . '/assets/css/twoinc.css', false, '1.2.1');
 }
 
 /**
@@ -99,7 +99,7 @@ function wc_twoinc_enqueue_styles()
  */
 function wc_twoinc_enqueue_scripts()
 {
-    wp_enqueue_script('twoinc-payment-gateway-js', WC_TWOINC_PLUGIN_URL . '/assets/js/twoinc.js', ['jquery'], '2.3.1');
+    wp_enqueue_script('twoinc-payment-gateway-js', WC_TWOINC_PLUGIN_URL . '/assets/js/twoinc.js', ['jquery'], '2.3.2');
 }
 
 /**
