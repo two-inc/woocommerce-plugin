2e72edf0c393dc63c287b60e56d6bf4d7e955aef
Bharat Kunwar <brtknr@bath.edu>
2021-12-20 14:00:58 +0000

    Ignore .gitattributes and remove get-zip.sh

--- get-zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-zip -r tillit-payment-gateway.zip 'assets' 'class' 'views' 'readme.txt' *.php *.pot *.mo *.po
-printf "\n\n"
-read -n 1 -s -r -p "Zipping completed. Please press any key to exit"
