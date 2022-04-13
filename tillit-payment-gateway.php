a3575f65ae9b89adb8c17f88a80835c503418ba3
harry <hnguyen@tillit.ai>
2022-04-13 19:47:47 +0700

    version number

--- tillit-payment-gateway.php
+++ tillit-payment-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: Two - BNPL for businesses
  * Plugin URI: https://two.inc
  * Description: Integration between WooCommerce and Two
- * Version: 2.8.3
+ * Version: 2.8.4
  * Author: Two
  * Author URI: https://two.inc
  * Text Domain: twoinc-payment-gateway
