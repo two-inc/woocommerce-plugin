5948031c65b79639f1b63e779ec72795471f878d
harry <hnguyen@tillit.ai>
2022-09-22 21:39:24 +0700

    t-6590/feat: aadalen add company default

--- snippets/aadalen.no/functions-part.php
+++ snippets/aadalen.no/functions-part.php
@@ -5,3 +5,13 @@ add_action( 'wp_footer', function () {
       echo '<script type="text/javascript" src="/wp-content/plugins/woocommerce/assets/js/selectWoo/selectWoo.full.min.js"></script>';
    }
 } );
+
+
+// Add default placeholder for company name
+function add_company_display_default( $fields ) {
+    if (array_key_exists('billing_company_display', $fields['billing'])) {
+        $fields['billing']['billing_company_display']['options'] = ['' => __('Company name', 'twoinc-payment-gateway')];
+    }
+    return $fields;
+}
+add_filter( "woocommerce_checkout_fields", "add_company_display_default", 30 );
