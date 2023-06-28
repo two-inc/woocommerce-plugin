177ee29e53507aac94028087c12812913b8f4ec0
harrynguyenquanghuy <hnguyen@two.inc>
2023-06-28 22:07:29 +0700

    t-10968/feat: add configurable due in days for buyer with UI updates

--- snippets/two-demo/functions-part.php
+++ snippets/two-demo/functions-part.php
@@ -13,9 +13,23 @@ add_filter("two_order_create", "add_two_order_fields");
 // Add the new field due in days in checkout
 function add_two_due_days($fields) {
 
+    $lang = WC_Twoinc_Helper::get_locale();
+    $label_text = "Days you'll have to pay your invoice";
+    if ($lang === 'sv_SE') {
+        $label_text = 'Dagar du kommer ha att betala din faktura';
+    } else if ($lang === 'nb_NO') {
+        $label_text = 'Dager du må betale fakturaen';
+    }
+
     $fields['billing']['billing_due_in_days'] = [
-        'label' => 'Number of due days',
-        'type' => 'text',
+        'label'    => $label_text,
+        'type'     => 'select',
+        'options'  => array(
+             '14'  => '14 days',
+             '30'  => '30 days',
+             '60'  => '60 days',
+             '90'  => '90 days'
+        ),
         'required' => true,
         'priority' => 30
     ];
@@ -23,3 +37,32 @@ function add_two_due_days($fields) {
     return $fields;
 }
 add_filter("woocommerce_checkout_fields", "add_two_due_days");
+
+
+// Add the javascript to replace due in days in UI
+function add_demo_replace_due_in_days_script() {
+?>
+
+<script type='text/javascript'>
+function replaceDueInDays() {
+    if (jQuery('#twoinc-due-in-days').length == 0) {
+        jQuery('label[for="payment_method_woocommerce-gateway-tillit"]').html(
+            jQuery('label[for="payment_method_woocommerce-gateway-tillit"]').html()
+                .replace(twoinc.days_on_invoice, '<span id="twoinc-due-in-days"></span>')
+        )
+    }
+    jQuery('#twoinc-due-in-days').text(jQuery('#billing_due_in_days').val())
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(replaceDueInDays, 1000)
+    setInterval(replaceDueInDays, 3000)
+    jQuery('#billing_due_in_days').on('change', function(){
+        replaceDueInDays()
+    })
+})
+</script>
+
+<?php
+}
+add_action('woocommerce_before_checkout_billing_form', 'add_demo_replace_due_in_days_script');
