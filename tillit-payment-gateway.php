ea20b6e92fbf69bbd6b1c4e598d6b91d632699e2
Bharat Kunwar <bkunwar@two.inc>
2024-12-06 14:43:54 +0000

    chore: Bump version 2.20.0 -> 2.20.1

--- tillit-payment-gateway.php
+++ tillit-payment-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: Two - BNPL for businesses
  * Plugin URI: https://two.inc
  * Description: Integration between WooCommerce and Two
- * Version: 2.20.0
+ * Version: 2.20.1
  * Author: Two
  * Author URI: https://two.inc
  * Text Domain: twoinc-payment-gateway
